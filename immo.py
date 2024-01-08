99e4b04bfb8d4813b6611bcac03a3c495392bb9f
Liesje123 <elisabeth.guitton@isen-ouest.yncrea.fr>
2024-01-08 10:14:19 +0100

    ✨Ajout dossier data et modification immo

--- immo.py
+++ immo.py
@@ -1,3 +1,12 @@
 # Lit fichier, traite le fichier (Pandas)
 
-#commentaire
+import pandas as pd 
+
+def liste_des_villes():
+    """ Retourne la liste de toutes les villes
+    :return : liste de string"""
+    
+    df = pd.read_csv('./data/prix_immobilier_fictif.csv')
+    
+    #liste des villes sans doublons
+    return df['Ville'].unique().tolist()
