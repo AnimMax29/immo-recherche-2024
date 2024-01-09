ac70cb97f4209a8bfe1869ffb2b73cbe96fe8f56
Liesje123 <elisabeth.guitton@isen-ouest.yncrea.fr>
2024-01-09 11:22:01 +0100

    ✨ Ajout front

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 #endpoint (Flash)
 # ceci est un commentaire ?
-from flask import Flask, jsonify, request
-from immo import liste_des_villes, liste_des_quartiers, quartiers_pour_ville
+from flask import Flask, jsonify, request, send_from_directory
+from immo import liste_des_villes, liste_des_quartiers, quartiers_pour_ville,prix_pour_quartier_et_ville
 
 app = Flask(__name__)
+
+@app.route('/')
+def serve_static_file():
+    return send_from_directory('static', 'index.html')
 
 @app.route('/villes', methods=['GET'])
 def get_all_villes():
@@ -11,7 +15,7 @@ def get_all_villes():
     print("appel de la requête - /villes")
     return jsonify(all_villes)
 
-@app.route('/quartier/all', methods=['GET'])
+@app.route('/quartiers', methods=['GET'])
 def get_all_quartiers():
     all_quartiers = liste_des_quartiers()
     print("appel de la requête - /quartiers/all")
@@ -22,6 +26,12 @@ def get_quartiers(ville):
     quartiers = quartiers_pour_ville(ville)
     print("appel de la requête - /quartiers/<string:ville> - avec la ville",ville)
     return jsonify(quartiers)
+
+@app.route('/prix/<string:ville>/<string:quartier>', methods=['GET'])
+def get_prix(ville,quartier):
+    prix= prix_pour_quartier_et_ville(ville,quartier)
+    print("appel de la requête - /prix/<string:ville>/<string:quartier> - avec la ville",ville,"- avec le quartier",quartier)
+    return jsonify(prix)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- immo.py
+++ immo.py
@@ -1,12 +1,14 @@
 # Lit fichier, traite le fichier (Pandas)
 
 import pandas as pd 
+fichier = './immo-recherche-2024/data/prix_immobilier_fictif.csv'
+#fichier = './data/prix_immobilier_fictif.csv'
 
 def liste_des_villes():
     """ Retourne la liste de toutes les villes
     :return : liste de string"""
     
-    df = pd.read_csv('./data/prix_immobilier_fictif.csv')
+    df = pd.read_csv(fichier)
     
     #liste des villes sans doublons
     print("liste des villes :",df['Ville'].unique().tolist())
@@ -16,7 +18,7 @@ def liste_des_quartiers():
     """ Retourne la liste de toutes les quartiers
     :return : liste de string"""
     
-    df = pd.read_csv('./data/prix_immobilier_fictif.csv')
+    df = pd.read_csv(fichier)
     
     #liste des quartier sans doublons
     print("liste des quartiers :",df['Quartier'].unique().tolist())
@@ -27,10 +29,23 @@ def quartiers_pour_ville(ville):
     :param ville: nom de la ville (string)
     :return : liste de string"""
     
-    df = pd.read_csv('./data/prix_immobilier_fictif.csv')
+    df = pd.read_csv(fichier)
     quartiers = df[df['Ville'] == ville]['Quartier'].unique().tolist()
     print("liste des quartiers de la ville",ville," :",quartiers)
     return quartiers
+
+def prix_pour_quartier_et_ville(ville,quartier):
+    """ Retourne le prix au m² d'un quartier donnée d'une ville donnée
+    :param ville: nom de la ville (string), quartier : nom du quartier (string)
+    :return : string"""
+    
+    df = pd.read_csv(fichier)
+    
+    prix = df[(df['Ville'] == ville) & (df['Quartier'] == quartier)]['Prix au m²']
+    if not prix.empty:
+        return 'Le quartier '+quartier+' de la ville '+ville+' possède un prix de '+str(prix.values[0])+' au mètres carrés'
+    else:
+        return "Prix non trouvé pour cette ville et ce quartier."
 
 # liste_villes = liste_des_villes()
 # liste_quartiers = liste_des_quartiers()
